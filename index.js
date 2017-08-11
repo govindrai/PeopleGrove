630315f82860cbb245b6b87ccc00d39f99aa7a0b
Govind Rai <raigovind93@gmail.com>
2017-08-10 19:43:12 -0700

    remove index route from backend

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ app.use(passport.session());
 app.use("/api/todos", todos);
 app.use("/auth", auth);
 
-app.get("/", (req, res) => {
-  res.json({ status: 200 });
-});
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
