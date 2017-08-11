6bb6d9d5c825ac7bcffa433f293568e9ad87ab5d
Govind Rai <raigovind93@gmail.com>
2017-08-10 21:29:27 -0700

    Add login with facebook

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,11 @@
 import React from "react";
 
 export default props => {
-  const nameAttribute = props.loggedIn ? "Logout" : "Login";
   return (
     <div>
       <h1>PeopleGrove Task Tracker</h1>
-      <button
-        type="submit"
-        name={nameAttribute}
-        onClick={() => props.onClickHandler(props.history)}
-      >
-        {nameAttribute}
+      <button type="submit" onClick={() => props.onClickHandler(props.history)}>
+        {props.loggedIn ? "Logout" : "Login with Facebook"}
       </button>
     </div>
   );
